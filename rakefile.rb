5e407f837433bf8ecb4697985ee6e1222e3d452a
Geert Fannes <geert.fannes@auro-technologies.com>
2018-05-04 15:37:27 +0200

    added uth

--- rakefile.rb
+++ rakefile.rb
@@ -3,10 +3,13 @@ task :prepare do
         unless File.exist?("ChaiScript/readme.md")
             sh "git submodule update --init ChaiScript"
         end
-        Dir.chdir("ChaiScript") do
-            sh "git checkout develop"
-            sh "git pull"
-        end
+    end
+end
+
+task :uth => :prepare do
+    Dir.chdir("extern/ChaiScript") do
+        sh "git checkout develop"
+        sh "git pull"
     end
 end
 
