baee307311f27d321cd5baa3d02966c1229400bd
Geert Fannes <geert.fannes@auro-technologies.com>
2019-01-22 00:06:47 +0100

    msvc fix

--- src/gubg/chai/File.hpp
+++ src/gubg/chai/File.hpp
@@ -68,19 +68,19 @@ inline void open_and_read(const std::string & filename, std::function<void (IStr
 inline std::string absolute(File f, const std::string & str)
 {
     std::filesystem::path path(str);
-    return std::filesystem::absolute(path);
+    return std::filesystem::absolute(path).string();
 }
 
 #define JA const std::string & 
-inline std::string join_2(File f, JA a1, JA a2) { return gubg::filesystem::combine(a1, a2); }
-inline std::string join_3(File f, JA a1, JA a2, JA a3) { return gubg::filesystem::combine({a1, a2, a3}); }
-inline std::string join_4(File f, JA a1, JA a2, JA a3, JA a4) { return gubg::filesystem::combine({a1, a2, a3, a4}); }
-inline std::string join_5(File f, JA a1, JA a2, JA a3, JA a4, JA a5) { return gubg::filesystem::combine({a1, a2, a3, a4, a5}); }
-inline std::string join_6(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6}); }
-inline std::string join_7(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7}); }
-inline std::string join_8(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7, JA a8) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7, a8}); }
-inline std::string join_9(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7, JA a8, JA a9) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7, a8, a9}); }
-inline std::string join_10(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7, JA a8, JA a9, JA a10) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7, a8, a9, a10}); }
+inline std::string join_2(File f, JA a1, JA a2) { return gubg::filesystem::combine(a1, a2).string(); }
+inline std::string join_3(File f, JA a1, JA a2, JA a3) { return gubg::filesystem::combine({a1, a2, a3}).string(); }
+inline std::string join_4(File f, JA a1, JA a2, JA a3, JA a4) { return gubg::filesystem::combine({a1, a2, a3, a4}).string(); }
+inline std::string join_5(File f, JA a1, JA a2, JA a3, JA a4, JA a5) { return gubg::filesystem::combine({a1, a2, a3, a4, a5}).string(); }
+inline std::string join_6(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6}).string(); }
+inline std::string join_7(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7}).string(); }
+inline std::string join_8(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7, JA a8) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7, a8}).string(); }
+inline std::string join_9(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7, JA a8, JA a9) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7, a8, a9}).string(); }
+inline std::string join_10(File f, JA a1, JA a2, JA a3, JA a4, JA a5, JA a6, JA a7, JA a8, JA a9, JA a10) { return gubg::filesystem::combine({a1, a2, a3, a4, a5, a6, a7, a8, a9, a10}).string(); }
 #undef JA
 
 inline bool mkdir(File f, const std::string & filename)
